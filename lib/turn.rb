47378baff4e1a934c7300ddf7fbfeb33e0d31d83
Gabriel Onike <onikegabriel@gmail.com>
2020-04-06 13:07:38 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,7 +8,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def turn(board, index, input_to_index, valid_move?)
+def turn(board, index, input_to_index, valid_move?, move)
     if !valid_move(board,index)
       return board
     end
